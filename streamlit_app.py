e6fdc53f4d25e410a726a29a39a76b9952b008f6
RolandNicolai <nicolai.roland@bonnier.dk>
2024-06-28 15:20:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,13 @@ table = st.secrets["table"]
 user_prompt = st.text_input("User prompt:")
 button = st.button("Generate")
 
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
 if button and user_prompt:
+    for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
     with st.spinner('Opretter query...'):
         time.sleep(3)
     queryModel_response = queryModel.generate_content(
