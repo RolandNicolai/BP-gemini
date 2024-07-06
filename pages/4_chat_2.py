356fd2d149a42e0798e44b58a3c3aedd0060fd61
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-06 11:28:05 +0200

    Update 4_chat_2.py

--- pages/4_chat_2.py
+++ pages/4_chat_2.py
@@ -3,7 +3,29 @@ from google.oauth2 import service_account
 import vertexai
 import streamlit as st
 
-st.title("ChatGPT-like clone (Vertex AI)")
+
+st.title("VertexAI assistant")
+
+# Set OpenAI API key from Streamlit secrets
+#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["vertexAI_service_account"]
+)
+
+
+vertexai.init(project=st.secrets["project"], location=st.secrets["location"], credentials=credentials)
+
+generation_config = {
+  "temperature": 0,
+
+  "max_output_tokens": 8192,
+  #"response_mime_type": "text/plain",
+}
+model = GenerativeModel(
+    "gemini-1.5-pro-001",
+    generation_config=generation_config,
+)
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
