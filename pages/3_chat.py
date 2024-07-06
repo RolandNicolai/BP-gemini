a9ad8d91cfa85ad6f216fc7c44789645e8eb28d0
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-06 12:27:44 +0200

    Update 3_chat.py

--- pages/3_chat.py
+++ pages/3_chat.py
@@ -62,7 +62,7 @@ chart_script_func = FunctionDeclaration(
         "properties": {
             "query": {
                 "type": "string",
-                "description": f"From the user prompt from the user question: {prompt} generate a Python script on a single line that will help give quantitative answers to the user's question. In the python script, always use the fully qualified dataframe df_cleaned and field names {fieldNames}.",
+                "description": f"Python script on a single line that will help give quantitative answers to the user's question. In the python script, always use the fully qualified dataframe df_cleaned and field names.",
             }
         },
         "required": [
