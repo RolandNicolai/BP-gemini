0ae87f7a6438260d91e4e5edfac89190aa68faa8
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-06 13:03:48 +0200

    Update 3_chat.py

--- pages/3_chat.py
+++ pages/3_chat.py
@@ -111,6 +111,7 @@ if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
     with st.chat_message("assistant"):
         response = st.session_state["vertex_model"].generate_content(prompt)
         st.markdown(response.text)
+        chat = model.start_chat()
         response = chat.send_message(prompt)
         response = response.candidates[0].content.parts[0]
         print(response)
