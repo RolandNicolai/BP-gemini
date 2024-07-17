8dd899315c8a0122d90b07d9c843bf51d2e03241
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-17 07:59:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,7 @@ for message in st.session_state.messages:
 
 # Handle user input
 if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
-    st.session_state.messages.append({"role": "user", avatar="🦖", "content": prompt})
+    st.session_state.messages.append({"role": "user", "avatar": "🦖", "content": prompt})
     with st.chat_message("user", avatar="🦖"):
         st.markdown(prompt)
 
