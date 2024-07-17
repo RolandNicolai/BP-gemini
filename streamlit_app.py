dba3f1c57d219f491b5d4d9c993cd8a4dff2debc
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-17 08:45:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,10 @@ st.subheader("Bygget på Google Gemini")
 st.subheader("Vælg en applikation")
 
 
-col1, col2, col3, col4 = st.columns([4, 1])
-with col1:
-    st.page_link("streamlit_app.py", label="Forside")
-with col2:
-    st.page_link("pages/1_Data_Assistent.py", label="Data Assistent")
-with col3:
-    st.page_link("pages/2_chat.py", label="Chat#2")
-with col4:
-    st.page_link("pages/3_chat.py", label="Chat#3")
+st.page_link("streamlit_app.py", label="Forside")
+st.page_link("pages/1_Data_Assistent.py", label="Data Assistent")
+st.page_link("pages/2_chat.py", label="Chat#2")
+st.page_link("pages/3_chat.py", label="Chat#3")
 
 
 
