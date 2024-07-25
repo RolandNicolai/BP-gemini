b51b436f091590790e03ee47d6d72da218208659
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-25 12:34:07 +0200

    Update 4_Chat_Viz.py

--- pages/4_Chat_Viz.py
+++ pages/4_Chat_Viz.py
@@ -271,7 +271,11 @@ if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
                             [response.function_call.name, params, api_response]
                         )
 
-
+                    except Exception as e:
+                        api_response = f"{str(e)}"
+                        api_requests_and_responses.append(
+                            [response.function_call.name, params, api_response]
+                        )
                 if response.function_call.name == "pyplot_script":
                     try:
 
