38790212cf2d2f0100108f73d13bc751ef15c46c
RolandNicolai <nicolai.roland@bonnier.dk>
2024-07-25 12:42:14 +0200

    Update 4_Chat_Viz.py

--- pages/4_Chat_Viz.py
+++ pages/4_Chat_Viz.py
@@ -288,6 +288,7 @@ if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
                             .replace("SQL:", "")
 
                         )
+                        execute_generated_code(plot_cleaned)
                         api_requests_and_responses.append(
                             [response.function_call.name, params, api_response]
                         )
