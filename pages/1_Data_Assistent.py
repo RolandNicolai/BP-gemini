f50ff4efee9d9babfbf94aed7008de79dc3586bc
RolandNicolai <nicolai.roland@bonnier.dk>
2024-08-15 11:06:01 +0200

    Update 1_Data_Assistent.py

--- pages/1_Data_Assistent.py
+++ pages/1_Data_Assistent.py
@@ -50,7 +50,7 @@ with st.expander("**Sample prompts for data**", expanded=True):
         - Hvor mange salg havde HIS i 2024 på mediekoden redteaser på owned channel
         - Hvor mange salg havde GDS i 2024 på mediekoden redteaser på owned channel i juni vs i maj
         - Hvilke 5 mediekoder havde flest salg i juni 2024
-        - Hvor mange salg havde hhv. GDS og HIS i juni?
+        - Hvor mange salg havde de to brands GDS og HIS i juni?
     """
     )
 
