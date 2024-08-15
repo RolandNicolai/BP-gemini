00650b2364553dc8fda59e6b07cfe9e335e579c0
RolandNicolai <nicolai.roland@bonnier.dk>
2024-08-15 10:44:28 +0200

    Update 2_Draft.py

--- pages/2_Draft.py
+++ pages/2_Draft.py
@@ -231,7 +231,7 @@ if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
                         reason = params['reason']
                         table_id = "bonnier-deliverables.LLM_vertex.LLM_QA"
                         rows_to_insert = [
-                            {"question": prompt, "reason": reason, "query": cleaned_query, "date": current_date_str}
+                            {"question": prompt, "reason": reason, "query": cleaned_query, "result": api_response , "date": current_date_str}
                         ]
                         errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
                         if errors == []:
