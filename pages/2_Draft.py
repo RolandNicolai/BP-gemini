9574b9de243c0bca25f789ec3cd624705a95aae5
RolandNicolai <nicolai.roland@bonnier.dk>
2024-08-15 10:03:33 +0200

    Update 2_Draft.py

--- pages/2_Draft.py
+++ pages/2_Draft.py
@@ -226,6 +226,7 @@ if prompt := st.chat_input("Hvad kan jeg hjælpe med?"):
                         
                         api_requests_and_responses.append(
                             [response.function_call.name, params, api_response]
+                        )
                         save_answer_query = (f"""INSERT INTO `bonnier-deliverables.LLM_vertex.LLM_QA`(question, reason, query, date) 
                                             VALUES ({prompt}, {params['reason']}, {cleaned_query}, {current_date_str})"""
                                             )
