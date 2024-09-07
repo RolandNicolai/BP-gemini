2dbe259b75db7ba56ba244d5f5e4f1fba2964fa4
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-08 00:28:28 +0200

    Update 1_Hjem.py

--- pages/1_Hjem.py
+++ pages/1_Hjem.py
@@ -38,13 +38,12 @@ quotes = [
     "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world. — Albert Einstein",
     "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. — Marie Curie",
     "If I have seen further, it is by standing on the shoulders of giants. — Isaac Newton",
-    "However difficult life may seem, there is always something you can do and succeed at. — Stephen Hawking",
     "I would rather have questions that can't be answered than answers that can't be questioned. — Richard Feynman",
-    "Music is a higher revelation than all wisdom and philosophy. — Ludwig van Beethoven",
-    "Neither a lofty degree of intelligence nor imagination nor both together go to the making of genius. Love, love, love, that is the soul of genius. — Wolfgang Amadeus Mozart",
     "It's easy to play any musical instrument: all you have to do is touch the right key at the right time and the instrument will play itself. — Johann Sebastian Bach",
     "Simplicity is the final achievement. After one has played a vast quantity of notes and more notes, it is simplicity that emerges as the crowning reward of art. — Frédéric Chopin",
     "Works of art make rules; rules do not make works of art. — Claude Debussy"
+    "A computer would deserve to be called intelligent if it could deceive a human into believing that it was human. - Alan Turing"
+    "The most dangerous phrase in the language is, ‘We’ve always done it this way.’ - Grace Hopper"
 ]
 
 # Function to get a random quote
@@ -53,9 +52,6 @@ def get_random_quote():
 
 # Streamlit app
 def main():
-    st.title("Random Quote Generator")
-    st.write("Here is a quote from a renowned scientist or classical musician:")
-
     # Display a random quote in st.caption
     random_quote = get_random_quote()
     st.caption(f"> {random_quote}")
