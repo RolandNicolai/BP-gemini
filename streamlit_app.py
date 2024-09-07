76aa3eccea08a0bd84fa6c93e9968f1d3e0d3063
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-08 01:13:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 hjem = st.Page("pages/1_Hjem.py", title="Hjem")
 data_assistent = st.Page("pages/2_Data_Assistent.py", title="Data Assistent")
 #create_page = st.Page("pages/1_Data_Assistent.py", title="Data Assistent", icon=":material/add_circle:")
-SQL_assistent = st.Page("pages/3_Marketing_SQL_Assistent.py", title="Artikel Assistent")
+SQL_assistent = st.Page("pages/3_Marketing_SQL_Assistent.py", title="Marketing SQL Assistent")
 pdf_assistent = st.Page("pages/4_PDF_Assistent.py", title="PDF Assistent")
 translation_engine = st.Page("pages/5_Translation_Engine.py", title="Translation Engine")
 
