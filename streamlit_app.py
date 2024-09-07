77db5f0fa17b7baea3de48b25681cd7f3041b82b
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-08 00:15:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 hjem = st.Page("pages/1_Hjem.py", title="Hjem")
 data_assistent = st.Page("pages/2_Data_Assistent.py", title="Data Assistent")
 #create_page = st.Page("pages/1_Data_Assistent.py", title="Data Assistent", icon=":material/add_circle:")
-artikel_assistent = st.Page("pages/3_Artikel_Assistent.py", title="Artikel Assistent", icon=":material/delete:")
+artikel_assistent = st.Page("pages/3_Artikel_Assistent.py", title="Artikel Assistent")
 
 pg = st.navigation([hjem, data_assistent, artikel_assistent])
 #st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
