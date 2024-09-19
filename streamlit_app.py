1f985077ce2c4974db312a6400aa274dd5578a8f
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-19 15:38:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ data_assistent = st.Page("pages/2_Data_Assistent.py", title="Data Assistent")
 SQL_assistent = st.Page("pages/3_Marketing_SQL_Assistent.py", title="Marketing SQL Assistent")
 pdf_assistent = st.Page("pages/4_PDF_Assistent.py", title="PDF Assistent")
 translation_engine = st.Page("pages/5_Translation_Engine.py", title="Translation Engine")
-alpha_data = st.page("6_data_assistent_draft.py", title = "data_ver_alpha")
+alpha_data = st.Page("6_data_assistent_draft.py", title = "data_ver_alpha")
 
 
 
