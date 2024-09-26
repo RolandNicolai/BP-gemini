c8f198c361aef35a1ed63647ad38030a36bc83bb
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-26 20:21:24 +0200

    Update 7_article_search_upload.py

--- pages/7_article_search_upload.py
+++ pages/7_article_search_upload.py
@@ -1,44 +1,38 @@
 import streamlit as st
-import base64
-from vertexai.generative_models import Part
 from google.oauth2 import service_account
-from vertexai.generative_models import FunctionDeclaration, GenerativeModel, Part, Tool
-import vertexai
-import time
-
-
+from google.cloud import storage
 
 LOGO_URL_LARGE = "https://bonnierpublications.com/app/themes/bonnierpublications/assets/img/logo.svg"
-st.logo(LOGO_URL_LARGE)
+st.image(LOGO_URL_LARGE)
 
 st.header('Analysér dine dokumenter med GenAI', divider='rainbow')
 
+# Setup Google Cloud credentials
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["vertexAI_service_account"]
 )
-from google.cloud import storage
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
+def upload_blob(bucket_name, file_obj, destination_blob_name):
+    """Uploads a file to the bucket from a file-like object."""
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-    blob.upload_from_file(source_file, content_type="application/pdf")
+    
+    # Upload the file directly from the file-like object
+    blob.upload_from_file(file_obj, content_type="application/pdf")
+    
     st.write(f"File uploaded to {bucket_name}/{destination_blob_name}.")
 
-# Replace these with your actual values
-bucket_name = "vertex_search_assets"
-#source_file_name = "/content/image.png"  # Replace with your local file path
-#destination_blob_name = "your_file_in_bucket.txt"  # Replace with the desired name in the bucket
+# Streamlit file uploader
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
-#source_file_name = st.file_uploader("vælg din fil", type="pdf")
-
-source_file = st.file_uploader("Choose a PDF file", type="pdf")
-
-
+# File name in cloud storage
 destination_blob_name = "test1.pdf"
 
-
+# Handle file upload when button is clicked
 if st.button("Upload fil"):
-    upload_blob(bucket_name, source_file_name, destination_blob_name)
-    #st.write((f"File {source_file_name} uploaded to {bucket_name}/{destination_blob_name}.")
+    if uploaded_file is not None:
+        # Call the upload_blob function with the file object
+        upload_blob("vertex_search_assets", uploaded_file, destination_blob_name)
+    else:
+        st.error("No file uploaded. Please upload a PDF file.")
