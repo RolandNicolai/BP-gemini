8751de1a71a134adb604a0d2a9d04c6b21b338c9
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-26 20:15:58 +0200

    Update 7_article_search_upload.py

--- pages/7_article_search_upload.py
+++ pages/7_article_search_upload.py
@@ -23,7 +23,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
+    blob.upload_from_filename(source_file_name, content_type="application/pdf")
 
     print(f"File {source_file_name} uploaded to {bucket_name}/{destination_blob_name}.")
 
