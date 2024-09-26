a814e3c6f9d7d723993bf3e03a7517e568147892
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-26 19:45:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,12 @@ SQL_assistent = st.Page("pages/3_Marketing_SQL_Assistent.py", title="Marketing S
 pdf_assistent = st.Page("pages/4_PDF_Assistent.py", title="PDF Assistent")
 translation_engine = st.Page("pages/5_Translation_Engine.py", title="Translation Engine")
 alpha_data = st.Page("pages/6_data_assistent_alpha.py", title = "data_ver_alpha")
+pdf_upload = st.page("pages/pages/7_article_search_upload.py", title = "PDF upload")
 
 
 
 
-pg = st.navigation([hjem, data_assistent, SQL_assistent, pdf_assistent, translation_engine, alpha_data])
+pg = st.navigation([hjem, data_assistent, SQL_assistent, pdf_assistent, translation_engine, alpha_data, pdf_upload])
 #st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
 
