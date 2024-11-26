901b735baf9292ebe1d4ebc96253e1af87e498e6
RolandNicolai <nicolai.roland@bonnier.dk>
2024-11-26 08:49:58 +0100

    Update 1_Hjem.py

--- pages/1_Hjem.py
+++ pages/1_Hjem.py
@@ -111,4 +111,19 @@ with st.popover("Om applikationen - Sikkerhed/Modeller"):
     #st.divider()
 
 
+st.html(
+    <!-- Widget JavaScript bundle -->
+<script src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_GB"></script>
+
+<!-- Search widget element is not visible by default -->
+<gen-search-widget
+  configId="d77c7c91-dd51-4eb4-8012-1d2353aa36e7"
+  location="eu"
+  triggerId="searchWidgetTrigger">
+</gen-search-widget>
+
+<!-- Element that opens the widget on click. It does not have to be an input -->
+<input placeholder="Search here" id="searchWidgetTrigger" />
+)
+
         
